989e27d304f6dc7c8516e28212b46c5a24b9b90a
gaspard <gaspard.dama@gmail.com>
2021-12-15 16:27:31 +0100

    Head is now displayed differently from the body

--- SnakeBody.java
+++ SnakeBody.java
@@ -2,4 +2,8 @@ public class SnakeBody extends Cell {
     public SnakeBody(int x, int y) throws InvalidNameException {
         super("█", x, y);
     }
+
+    public SnakeBody(String name, int x, int y) throws InvalidNameException {
+        super(name, x, y);
+    }
 }
